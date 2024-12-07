0d3e4867c310c9962f688c741f474d8850e51fc6
echchabli hamza <echchablihamza1@gmail.com>
2024-12-07 21:41:39 +0100

    project structer

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './index.html',              // Include root HTML file
+    './js/script.js',  // Include all HTML files in 'pages' directory and subdirectories
+    ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
